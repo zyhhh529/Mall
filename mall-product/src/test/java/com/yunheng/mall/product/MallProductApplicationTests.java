d87c7ea94fc263b09c7ba4aa8be52421d64b9b8a
Yun <yunhengz529@gmail.com>
2022-06-22 00:16:37 -0500

    Initial commit

---
+++ mall-product/src/test/java/com/yunheng/mall/product/MallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunheng.mall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
